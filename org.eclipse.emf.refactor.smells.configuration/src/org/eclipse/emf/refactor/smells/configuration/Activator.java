1fb39b7da67c51f0e006f8884840ad6f783cf2d1
tarendt <tarendt>
2012-10-11 20:07:31 +0000

    ConfigurationManager initialized

--- org.eclipse.emf.refactor.smells.configuration/src/org/eclipse/emf/refactor/smells/configuration/Activator.java
+++ org.eclipse.emf.refactor.smells.configuration/src/org/eclipse/emf/refactor/smells/configuration/Activator.java
@@ -1,5 +1,6 @@
 package org.eclipse.emf.refactor.smells.configuration;
 
+import org.eclipse.emf.refactor.smells.configuration.managers.ConfigurationManager;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -27,6 +28,7 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		ConfigurationManager.getInstance();
 	}
 
 	/*
