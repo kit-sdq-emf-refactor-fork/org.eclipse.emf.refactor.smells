980c05c557ed27d08ed97784e3dd3d3125966dc5
Misha Strittmatter <strittmatter@kit.edu>
2019-03-04 10:00:36 +0100

    decouple analysis from UI thread

--- org.eclipse.emf.refactor.smells.runtime/src/org/eclipse/emf/refactor/smells/runtime/handler/FindModelSmellHandler.java
+++ org.eclipse.emf.refactor.smells.runtime/src/org/eclipse/emf/refactor/smells/runtime/handler/FindModelSmellHandler.java
@@ -27,67 +27,112 @@ import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.emf.refactor.smells.runtime.managers.RuntimeManager;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
+
 public class FindModelSmellHandler extends AbstractHandler {
 
-	private static final String ECORE_EXTENSION = "ecore";
-	private static final Shell SHELL = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-	private ResourceSet resourceSet;
+    private static final String ECORE_EXTENSION = "ecore";
+    private static final Shell SHELL = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+    private ResourceSet resourceSet;
 
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		TransactionalEditingDomain transactionalEditingDomain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain();
-		resourceSet = transactionalEditingDomain.getResourceSet();
-		
-		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-		
-		if (!(selection instanceof IStructuredSelection))
-			throw new ExecutionException("Selection is not instance of IStructuredSelection and could not be handled");
-		
+    private static Object monitor = new Object();
+    private static volatile boolean running = false;
 
-		List<?> selectedElementsList = ((IStructuredSelection) selection).toList();
-		
-		Cursor oldCursor = SHELL.getCursor();
-		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().setCursor(new Cursor(null,SWT.CURSOR_WAIT));
-		Set<EPackage> toAnalyze = new HashSet<EPackage>();
-		
-		List<EPackage> selectedEPackages = selectedElementsList.stream().filter(o -> o instanceof EPackage).map(o -> (EPackage) o).collect(Collectors.toList());
-		toAnalyze.addAll(selectedEPackages);
-		
-		List<IContainer> selectedContainers = selectedElementsList.stream().filter(o -> o instanceof IContainer).map(o -> (IContainer) o).collect(Collectors.toList());
-		for (IContainer container : selectedContainers) {
-			try {
-				toAnalyze.addAll(getRootEPackages(container));
-			} catch (CoreException e) {
-				e.printStackTrace();
-			}
-		}
-		
-		List<IFile> selectedFiles = selectedElementsList.stream().filter(o -> o instanceof IFile).map(o -> (IFile) o).collect(Collectors.toList());
-		selectedFiles.stream().forEach(file -> toAnalyze.add(getRootEPackageFromEcoreFile(file)));
-		
-		EPackage elm = toAnalyze.iterator().next();
-		String path = elm.eResource().getURI().toPlatformString(true);
-		IFile selectedFile = (IFile) ResourcesPlugin.getWorkspace().getRoot().findMember(path);
-		IProject selectedProject = selectedFile.getProject();
-		
-		
-		EPackage dummyPackage = EcoreFactory.eINSTANCE.createEPackage();
-		dummyPackage.getESubpackages().addAll(toAnalyze);
-		
-		RuntimeManager.findConfiguredModelSmells(selectedProject, dummyPackage, selectedFile);
-		SHELL.setCursor(oldCursor);
-		disposeResourceSet(resourceSet);
-		return null;
-		
-	}
-	
-    public void disposeResourceSet(ResourceSet resourceSet) {
+    @Override
+    public Object execute(ExecutionEvent event) {
+
+        // start async execution
+        new Thread() {
+            @Override
+            public void run() {
+
+                // allow only single threaded execution
+                synchronized (monitor) {
+                    while (running) {
+                        try {
+                            this.wait();
+                        } catch (InterruptedException e) {
+                            prompt("A waiting job was interrupted.");
+                            e.printStackTrace();
+                        }
+                    }
+                    running = true;
+                }
+
+                try {
+                    doTheWork();
+                } catch (ExecutionException e) {
+                    prompt("An unexpected exception occured: " + e.getMessage());
+                    e.printStackTrace();
+                }
+
+                // release next waiting job if any
+                synchronized (monitor) {
+                    running = false;
+                    monitor.notifyAll();
+                }
+            }
+        }.start();
+
+        return null;
+    }
+
+    private void prompt(String message) {
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
+        MessageDialog.openInformation(shell, "EMF Refactor", message);
+        System.out.println("EMF Refactor prompt" + message);
+    }
+
+    private void doTheWork() throws ExecutionException {
+        TransactionalEditingDomain transactionalEditingDomain = TransactionalEditingDomain.Factory.INSTANCE.createEditingDomain();
+        resourceSet = transactionalEditingDomain.getResourceSet();
+
+        ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+
+        if (!(selection instanceof IStructuredSelection))
+            throw new ExecutionException("Selection is not instance of IStructuredSelection and could not be handled");
+
+        List<?> selectedElementsList = ((IStructuredSelection) selection).toList();
+
+        Cursor oldCursor = SHELL.getCursor();
+        PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().setCursor(new Cursor(null, SWT.CURSOR_WAIT));
+        Set<EPackage> toAnalyze = new HashSet<EPackage>();
+
+        List<EPackage> selectedEPackages = selectedElementsList.stream().filter(o -> o instanceof EPackage).map(o -> (EPackage) o).collect(Collectors.toList());
+        toAnalyze.addAll(selectedEPackages);
+
+        List<IContainer> selectedContainers = selectedElementsList.stream().filter(o -> o instanceof IContainer).map(o -> (IContainer) o).collect(Collectors.toList());
+        for (IContainer container : selectedContainers) {
+            try {
+                toAnalyze.addAll(getRootEPackages(container));
+            } catch (CoreException e) {
+                e.printStackTrace();
+            }
+        }
+
+        List<IFile> selectedFiles = selectedElementsList.stream().filter(o -> o instanceof IFile).map(o -> (IFile) o).collect(Collectors.toList());
+        selectedFiles.stream().forEach(file -> toAnalyze.add(getRootEPackageFromEcoreFile(file)));
+
+        EPackage elm = toAnalyze.iterator().next();
+        String path = elm.eResource().getURI().toPlatformString(true);
+        IFile selectedFile = (IFile) ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+        IProject selectedProject = selectedFile.getProject();
+
+        EPackage dummyPackage = EcoreFactory.eINSTANCE.createEPackage();
+        dummyPackage.getESubpackages().addAll(toAnalyze);
+
+        RuntimeManager.findConfiguredModelSmells(selectedProject, dummyPackage, selectedFile);
+        SHELL.setCursor(oldCursor);
+        disposeResourceSet(resourceSet);
+    }
+
+    private void disposeResourceSet(ResourceSet resourceSet) {
         // unload and delete all resources
         Iterator<Resource> iterator = resourceSet.getResources().iterator();
         while (iterator.hasNext()) {
@@ -97,47 +142,41 @@ public class FindModelSmellHandler extends AbstractHandler {
         }
     }
 
-	private Collection<EPackage> getRootEPackages(IContainer container) throws CoreException {
-		List<IFile> ecoreFiles = getEcoreFiles(container);
-		List<EPackage> ePackages = ecoreFiles.stream().map(elm -> getRootEPackageFromEcoreFile(elm)).collect(Collectors.toList());
-		return ePackages;
-	}
+    private Collection<EPackage> getRootEPackages(IContainer container) throws CoreException {
+        List<IFile> ecoreFiles = getEcoreFiles(container);
+        List<EPackage> ePackages = ecoreFiles.stream().map(elm -> getRootEPackageFromEcoreFile(elm)).collect(Collectors.toList());
+        return ePackages;
+    }
 
+    private EPackage getRootEPackageFromEcoreFile(IFile ecoreFile) {
+        URI uri = URI.createPlatformResourceURI(ecoreFile.getFullPath().toString(), true);
+        resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(ECORE_EXTENSION, new EcoreResourceFactoryImpl());
+        Resource resource = resourceSet.getResource(uri, true);
+        EcoreUtil.resolveAll(resource);
+        return (EPackage) resource.getContents().get(0);
+    }
 
-	private EPackage getRootEPackageFromEcoreFile(IFile ecoreFile) {
-		URI uri = URI.createPlatformResourceURI(ecoreFile.getFullPath().toString(), true); 
-		resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put(ECORE_EXTENSION, new EcoreResourceFactoryImpl());
-		Resource resource = resourceSet.getResource(uri, true);
-		EcoreUtil.resolveAll(resource);
-		return (EPackage) resource.getContents().get(0);
-	}
+    private List<IFile> getEcoreFiles(IContainer container) throws CoreException {
+        List<IFile> result = new ArrayList<IFile>();
+        for (IResource r : container.members()) {
+            if (r instanceof IContainer)
+                result.addAll(getEcoreFiles((IContainer) r));
+            else if ((r instanceof IFile) && r.getFileExtension().equals(ECORE_EXTENSION))
+                result.add((IFile) r);
+        }
+        return result;
+    }
 
-	private List<IFile> getEcoreFiles(IContainer container) throws CoreException {
-		List<IFile> result = new ArrayList<IFile>();
-		for (IResource r : container.members()) {
-			if (r instanceof IContainer)
-				result.addAll(getEcoreFiles((IContainer) r));
-			else if ((r instanceof IFile) && r.getFileExtension().equals(ECORE_EXTENSION))
-				result.add((IFile) r);
-		}
-		return result;
-	}
+    @Override
+    public boolean isEnabled() {
+        ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
+        if (!(selection instanceof IStructuredSelection))
+            return false;
 
+        List<?> selectedElementsList = ((IStructuredSelection) selection).toList();
 
-	@Override
-	public boolean isEnabled() {
-		ISelection selection = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getSelectionService().getSelection();
-		if (!(selection instanceof IStructuredSelection))
-			return false;
-		
+        return selectedElementsList.stream()
+                .allMatch(elm -> (elm instanceof EObject) || (elm instanceof IContainer) || ((elm instanceof IFile) && ((IFile) elm).getFileExtension().equals(ECORE_EXTENSION)));
+    }
 
-		List<?> selectedElementsList = ((IStructuredSelection) selection).toList();
-		
-
-		return selectedElementsList.stream().allMatch(elm -> (elm instanceof EObject) ||
-				(elm instanceof IContainer) ||
-				((elm instanceof IFile) && ((IFile) elm).getFileExtension().equals(ECORE_EXTENSION)));
-	}
-
-	
 }
